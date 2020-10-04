1036ee67240ae2186e498d05c1682c6bdeffa30b
yourname <1850094299@qq.com>
2020-10-04 17:28:53 +0800

    test

--- leetcode_C/aaa.cpp
+++ leetcode_C/aaa.cpp
@@ -1,19 +1,43 @@
 #include<stack>
 #include<queue>
-
 bool check_is_valid_order(std::queue<int>& order) {
-
-	std::stack<int> S; 
+	std::stack<int>S;
 	int n = order.size();
-	for (int i = 01; i <= n; i++) {
+	for (int i = 0; i <= n; i++) {
 		S.push(i);
-		while (!S.empty()&&order.front()==S.top()) {
+		while (!S.empty() && order.front() == S.top()) {
 			S.pop();
 			order.pop();
 		}
 	}
-	if () {
-		reture false;
+	if (!S.empty() ){
+		return false;
 	}
 	return true;
+}
+
+int main() {
+	int n;
+	int train;
+	scanf("%d", &n);
+	while (n) {
+		scanf("%d", &train);
+		while (train) {
+			std::queue<int>order;
+			for (int i = 1; i < n; i++) {
+				scanf("%d", &train);
+				order.push(train);
+			}
+			if (check_is_valid_order(order)) {
+				printf("Yes\n");
+			}
+			else {
+				printf("No\n");
+			}
+			scanf("%d", &train);
+		}
+		printf("\n");
+		scanf("%d", &n);
+	}
+	return 0;
 }
