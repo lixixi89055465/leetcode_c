a26fb205218d3671a1c8ddd367f38e8357a517a4
1850094299@qq.com <1850094299@qq.com>
2020-10-20 23:25:43 +0800

    init

--- base/base.cpp
+++ base/base.cpp
@@ -5,34 +5,38 @@
 #include "stdio.h"
 #include "vector"
 
-struct ListNode{
+struct ListNode {
     int val;
-    ListNode*next;
-    ListNode(int x):val(x),next(NULL){}
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
-void add_to_vector(ListNode*head,std::vector<int>&vec){
-    if(vec.empty()){
+void add_to_vector(ListNode *head, std::vector<int> &vec) {
+    if (NULL == head) {
         return;
     }
+    vec.push_back(head->val);
+    add_to_vector(head->next, vec);
 }
-int main(){
+
+int main() {
     ListNode a(1);
     ListNode b(2);
     ListNode c(3);
     ListNode d(4);
     ListNode e(5);
 
-    a.next=&b;
-    b.next=&c;
-    c.next=&d;
-    d.next=&e;
-    std::vector<int>vec;
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+    std::vector<int> vec;
 
-    add_to_vector(&a,vec);
+    add_to_vector(&a, vec);
 
-    for(int i=0;i<vec.size();i++){
-        printf("[%d]",vec[i]);
+    for (int i = 0; i < vec.size(); i++) {
+        printf("[%d]", vec[i]);
     }
     printf("\n");
     print
