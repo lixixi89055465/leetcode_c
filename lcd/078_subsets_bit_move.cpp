b8aba17e04a5f981877e889bcdbdbfb905cfa871
1850094299@qq.com <1850094299@qq.com>
2020-10-21 18:45:32 +0800

    递归

--- lcd/078_subsets_bit_move.cpp
+++ lcd/078_subsets_bit_move.cpp
@@ -6,10 +6,10 @@
 
 class Solution {
 public:
-    std::vector<std::vector<int>> subsets(std::vector<int> &nums) {
+    std::vector<std::vector<int>> subSet(std::vector<int> &nums) {
+        int subSet = 1 << nums.size();
         std::vector<std::vector<int>> result;
-        int all_set = 1 << nums.size();
-        for (int i = 0; i < all_set; i++) {
+        for (int i = 0; i < subSet; i++) {
             std::vector<int> item;
             for (int j = 0; j < nums.size(); j++) {
                 if (i & (1 << j)) {
@@ -24,21 +24,15 @@ public:
 
 int main() {
     std::vector<int> nums;
+    nums.push_back(0);
     nums.push_back(1);
     nums.push_back(2);
-    nums.push_back(3);
-
-    std::vector<int> item;
-    std::vector<std::vector<int>> result;
-    for (int i = 0; i < nums.size(); i++) {
-        item.push_back(nums[i]);
-        result.push_back(item);
-    }
+    Solution solve;
+    std::vector<std::vector<int>> result = solve.subSet(nums);
     for (int i = 0; i < result.size(); i++) {
-        printf("\n");
         for (int j = 0; j < result[i].size(); j++) {
             printf("[%d]", result[i][j]);
         }
+        printf("\n");
     }
-    return 0;
 }
