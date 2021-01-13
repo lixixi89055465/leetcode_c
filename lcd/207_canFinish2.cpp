21ee8d7eab5e0dbea826ad7c3e7e0c654694ce93
lixiang <1850094299@qq.com>
2021-01-13 20:27:24 +0800

    判断图是否有环

--- lcd/207_canFinish2.cpp
+++ lcd/207_canFinish2.cpp
@@ -18,21 +18,6 @@ const int MAX_N = 5;
 class Solution {
 public:
 
-    //节点访问状态，-1没有访问过，0代表正在访问，1代表已完成访问
-    bool DFS_graph(GraphNode *node, int visit[]) {
-        visit[node->label] = 0;
-        for (int i = 0; i < node->neightbors.size(); i++) {
-            if (visit[node->neightbors[i]->label] == -1) {
-                if (DFS_graph(node->neightbors[i], visit) == 0) {
-                    return false;
-                }
-            } else if (visit[node->neightbors[i]->label] == 0) {
-                return false;
-            }
-        }
-        visit[node->label] = 1;
-        return true;
-    }
 
     bool canFinish(int numCourses,
                    std::vector<std::pair<int, int>> &prerequisites) {
@@ -46,7 +31,8 @@ public:
             GraphNode *begin = graph[prerequisites[i].second];
             GraphNode *end = graph[prerequisites[i].first];
             begin->neightbors.push_back(end);
-            degree[end->label]++;
+//            degree[end->label]++;
+            degree[prerequisites[i].first]++;
         }
         std::queue<GraphNode *> Q;
         for (int i = 0; i < numCourses; i++) {
@@ -81,29 +67,12 @@ public:
 
 
 int main() {
-    const int MAX_N = 5;
-    GraphNode *Graph[MAX_N];//5个顶点
-    for (int i = 0; i < MAX_N; i++) {
-        Graph[i] = new GraphNode(i);
-    }
-    Graph[0]->neightbors.push_back(Graph[4]);
-    Graph[0]->neightbors.push_back(Graph[2]);
-    Graph[1]->neightbors.push_back(Graph[0]);
-    Graph[1]->neightbors.push_back(Graph[2]);
-    Graph[2]->neightbors.push_back(Graph[3]);
-    Graph[3]->neightbors.push_back(Graph[4]);
-    Graph[4]->neightbors.push_back(Graph[3]);
-    int visit[MAX_N] = {0};
-
-    for (int i = 0; i < MAX_N; i++) {
-        if (visit[i] == 0) {
-            printf("From label ( %d ) ", Graph[i]->label);
-            DFS_graph(Graph[i], visit);
-            printf("\n");
-        }
-    }
-    for (int i = 0; i < MAX_N; i++) {
-        delete Graph[i];
-    }
+    std::vector<std::pair<int, int>> prerequisites;
+    prerequisites.push_back(std::make_pair(0, 0));
+    prerequisites.push_back(std::make_pair(1, 0));
+    prerequisites.push_back(std::make_pair(2, 1));
+    prerequisites.push_back(std::make_pair(3, 2));
+    Solution solution;
+    printf("%d\n", solution.canFinish(4, prerequisites));
     return 0;
 }
