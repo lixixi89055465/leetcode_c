e1eb365e4cd89c1c888d178b1bdbcd7d19e2b928
lixiang <1850094299@qq.com>
2021-02-22 14:56:05 +0800

    小岛的数量

--- lcd/200_number_of_islands.cpp
+++ lcd/200_number_of_islands.cpp
@@ -3,10 +3,39 @@
 //
 #include "vector"
 #include "string"
+#include "queue"
 
 class Solution {
 public:
     int numIslands(std::vector<std::vector<char>> &grid) {
+
+    }
+
+    void BFS(std::vector<std::vector<int>> &mark,
+             std::vector<std::vector<char>> &grid,
+             int x, int y) {
+        static const int dx[] = {-1, 1, 0, 0};
+        static const int dy[] = {0, 0, -1, 1};
+        std::queue<std::pair<int, int>> Q;// 宽度优先搜索队列
+        Q.push(std::make_pair(x, y));
+        mark[x][y] = 1;
+        while (!Q.empty()) {
+            x = Q.front().first;
+            y = Q.front().second;
+            Q.pop();
+            for (int i = 0; i < 4; i++) {
+                int newx = dx[i] + x;
+                int newy = dy[i] + y;
+                if (newx < 0 || newx >= mark.size() ||
+                    newy < 0 || newy >= mark[newx].size()) {
+                    continue;
+                }
+                if (mark[newx][newy] == 0 && grid[newx][newy] == '1') {
+                    Q.push(std::make_pair(newx, newy));
+                    mark[newx][newy] = 1;
+                }
+            }
+        }
 
     }
 
