bf8bde825946ffaf48e0d8fe9e736d3f2e3c1f77
lixiang <1850094299@qq.com>
2021-03-01 16:16:02 +0800

    动态规划 -  最大连续子数组

---
+++ lcd/down_100/053_maximum-subarray.cpp
@@ -0,0 +1,37 @@
+//
+// Created by nanji on 2/28/21.
+//
+
+#include "vector"
+
+class Solution {
+public:
+    int maxSubArray(std::vector<int> &nums) {
+        std::vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int max_res = dp[0];
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
+            if (dp[i] > max_res) {
+                max_res = dp[i];
+            }
+        }
+        return max_res;
+    }
+};
+
+int main() {
+    Solution solve;
+    std::vector<int> nums;
+    nums.push_back(-2);
+    nums.push_back(1);
+    nums.push_back(-3);
+    nums.push_back(4);
+    nums.push_back(-1);
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(-5);
+    nums.push_back(4);
+    printf("%d\n", solve.maxSubArray(nums));
+    return 0;
+}
