94aaa15a03d6a3f9931d6dd17b62a9f1a7418efc
lixiang <1850094299@qq.com>
2021-03-06 18:11:28 +0800

    复杂数据结构 -  实现 Trie 树

--- lcd/down_200/208_implement-trie-prefix-tree-medium.cpp
+++ lcd/down_200/208_implement-trie-prefix-tree-medium.cpp
@@ -119,46 +119,37 @@ private:
 };
 
 int main() {
-    TrieNode root;
-    TrieNode n1;
-    TrieNode n2;
-    TrieNode n3;
-    root.child['a' - 'a'] = &n1;
-    root.child['b' - 'a'] = &n2;
-    root.child['e' - 'a'] = &n3;
-    n2.is_end = true;
+    TrieTree trie_tree;
+    trie_tree.insert("abcd");
+    trie_tree.insert("abc");
+    trie_tree.insert("abd");
+    trie_tree.insert("b");
+    trie_tree.insert("bcd");
+    trie_tree.insert("efg");
+    printf("preorder_trie :\n");
+    preorder_trie(trie_tree.root(), 0);
 
-    TrieNode n4;
-    TrieNode n5;
-    TrieNode n6;
-    n1.child['b' - 'a'] = &n4;
-    n2.child['c' - 'a'] = &n5;
-    n3.child['f' - 'a'] = &n6;
+    printf("\n");
 
-    TrieNode n7;
-    TrieNode n8;
-    TrieNode n9;
-    TrieNode n10;
-    n4.child['c' - 'a'] = &n7;
-    n4.child['d' - 'a'] = &n8;
-    n5.child['d' - 'a'] = &n9;
-    n6.child['g' - 'a'] = &n10;
-    TrieNode n11;
-    n7.child['d' - 'a'] = &n11;
-    n2.is_end = true;
-    n7.is_end = true;
-    n8.is_end = true;
-    n9.is_end = true;
-    n10.is_end = true;
-    n11.is_end = true;
-
-//    preorder_trie(&root, 1);
-
-    std::vector<std::string> wordlist;
+    std::vector<std::string> word_list;
     std::string word;
-    get_all_word_from_trie(&root, word, wordlist);
-    for (int i = 0; i < wordlist.size(); i++) {
-        printf("%s\n", wordlist[i]);
+
+    printf("All words:\n");
+    get_all_word_from_trie(trie_tree.root(), word, word_list);
+    for (int i = 0; i < word_list.size(); i++) {
+        printf("%s\n", word_list[i].c_str());
     }
+    printf("\n");
+    printf("Search \n");
+    printf("abc : %d\n", trie_tree.search("abc"));
+    printf("abcd : %d\n", trie_tree.search("abcd"));
+    printf("bc: %d\n", trie_tree.search("bc"));
+    printf("b: %d\n", trie_tree.search("b"));
+
+    printf("\n");
+    printf("ab : %d\n", trie_tree.startsWith("ab"));
+    printf("abc : %d\n", trie_tree.startsWith("abc"));
+    printf("bc: %d\n", trie_tree.startsWith("bc"));
+    printf("fg : %d\n", trie_tree.startsWith("fg"));
     return 0;
 }
