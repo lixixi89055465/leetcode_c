f6e7e1792bd1ba20c9d6529ec5b5b3ca0ff73bc2
lixiang <1850094299@qq.com>
2021-03-07 10:51:20 +0800

    复杂数据结构 -  实现 Trie 树

---
+++ lcd/down_200/211_add-and-search-word-data-structure-design-medium.cpp
@@ -0,0 +1,21 @@
+//
+// Created by nanji on 3/6/21.
+//
+
+#include "stdio.h"
+#include "string"
+
+class WordDictionary {
+public:
+    WordDictionary() {
+
+    }
+
+    void addWord(std::string word) {
+
+    }
+
+    bool search(std::string word) {
+
+    }
+};
