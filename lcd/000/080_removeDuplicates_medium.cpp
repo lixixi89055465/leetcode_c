663a0d2df1873718d48223542a981eee95e1391f
lixiang <1850094299@qq.com>
2021-04-06 10:20:58 +0800

    88. 移除2次以上重复的元素

---
+++ lcd/000/080_removeDuplicates_medium.cpp
@@ -0,0 +1,79 @@
+//
+// Created by nanji on 4/6/21.
+/**
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+ 
+
+说明：
+
+为什么返回数值是整数，但输出的答案是数组呢？
+
+请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+你可以想象内部操作如下:
+
+// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+int len = removeDuplicates(nums);
+
+// 在函数里修改输入数组对于调用者是可见的。
+// 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+ 
+
+示例 1：
+
+输入：nums = [1,1,1,2,2,3]
+输出：5, nums = [1,1,2,2,3]
+解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。 不需要考虑数组中超出新长度后面的元素。
+示例 2：
+
+输入：nums = [0,0,1,1,1,1,2,3,3]
+输出：7, nums = [0,0,1,1,2,3,3]
+解释：函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。 不需要考虑数组中超出新长度后面的元素。
+
+ */
+//
+#include "vector"
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int> &nums) {
+        int num1 = 0;
+        std::vector<int>::iterator it = nums.begin();
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                num1++;
+            } else {
+                num1 = 0;
+            }
+            if(num1 >= 2) {
+                nums.erase(it + i + 1);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
+int main() {
+//    std::vector<int> vec = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+//    std::vector<int> vec = {1, 1, 1, 2, 2, 3};
+    std::vector<int> vec = {1, 1, 1};
+    printf("%d\n", vec.size());
+//    std::vector<int>::iterator itc = vec.begin();
+//    itc = vec.erase(itc + 3);
+//    printf("%d\n", *itc);
+//    vec.erase(itc + 2);
+//    printf("%d\n", *itc);
+//    printf("%d\n", vec.size());
+    Solution solve;
+    int result = solve.removeDuplicates(vec);
+    printf("%d\n", result);
+
+    return 0;
+}
